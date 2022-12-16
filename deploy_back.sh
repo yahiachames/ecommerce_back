68c4985457426be051df0d28ca3bb73a4b3a5244
yahiachames <yahiachames@gmail.com>
2022-12-16 02:11:54 +0100

    fix apache

--- deploy_back.sh
+++ deploy_back.sh
@@ -4,8 +4,10 @@ apt update
 apt-get -y install python3-pip && apt-get -y install python3-venv
 apt-get install -y apt-utils vim curl apache2 apache2-utils
 apt-get -y install libapache2-mod-wsgi-py3
+pip install -r requirements.txt
 cp django_project.conf /etc/apache2/sites-available/
 a2ensite django_project 
 a2dissite 000-default.conf
+a2enmod rewrite ssl
+systemctl restart apache2
 pip install -r requirements.txt
-chmod 777 /app/media_root
