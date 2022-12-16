969f95801da7fcc9d8930bbafb07b4b9d8e11561
yahiachames <yahiachames@gmail.com>
2022-12-16 02:16:41 +0100

    fix apache

--- deploy_back.sh
+++ deploy_back.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 apt update
-apt-get -y install python3-pip && apt-get -y install python3-venv
+apt-get -y install python3-pip && apt-get -y install python3-venv && apt-get install python3-dev libmysqlclient-dev
 apt-get install -y apt-utils vim curl apache2 apache2-utils
 apt-get -y install libapache2-mod-wsgi-py3
 pip install -r requirements.txt
@@ -10,4 +10,4 @@ a2ensite django_project
 a2dissite 000-default.conf
 a2enmod rewrite ssl
 systemctl restart apache2
-pip install -r requirements.txt
+
